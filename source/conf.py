e773a485dd53787ca687b241fac30d6723782511
Sebastian Pfaffel <sebastian.pfaffel@iee.fraunhofer.de>
2019-07-15 19:09:33 +0200

    3. commit

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,8 @@ release = '1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['nbsphinx',
+    'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
